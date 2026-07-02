a2307de045c82a384442870f08d6ce21b699efa3
Devashermale <135997014+Devashermale@users.noreply.github.com>
2026-07-02 12:34:21 +0530

    Fix import path for AuthContextProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthContextProvider } from './context/AuthContext.jsx'
+import { AuthContextProvider } from './context/Authcontext.jsx'
 import { ExamContextProvider } from './context/ExamContext.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
